9b3a1281c23fba0be53d2035c49d6dbeda040574
vtsitlak <vtsitlak@gmail.com>
2020-01-27 21:44:33 +0100

    filter form component and test

--- src/app/vehicles/vehicles/filter-form/filter-form.component.spec.ts
+++ src/app/vehicles/vehicles/filter-form/filter-form.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { FilterFormComponent } from './filter-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FilterFormComponent', () => {
   let component: FilterFormComponent;
@@ -8,9 +10,10 @@ describe('FilterFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterFormComponent ]
+      imports: [BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MatSelectModule],
+      declarations: [FilterFormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +22,29 @@ describe('FilterFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create FilterFormComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should enable form when having available types or brands or colors', () => {
+    component.types = ['car'];
+    component.brands = ['citroen'];
+    component.colors = ['silver'];
+    component.ngOnChanges();
+    expect(component.filtersForm.enabled).toBeTruthy();
+  });
+
+  it('should disable form not having available types or brands or colors', () => {
+    component.types = ['car'];
+    component.brands = undefined;
+    component.colors = ['silver'];
+    component.ngOnChanges();
+    expect(component.filtersForm.enabled).toBeFalsy();
+  });
+
+  it('should emit new filter value when form values changinhg', fakeAsync(() => {
+    spyOn(component.updateFilter, 'emit');
+    component.filtersForm.setValue({ type: 'car', brand: '', color: 'red' });
+    expect(component.updateFilter.emit).toHaveBeenCalledWith({ type: 'car', brand: '', color: 'red' });
+  }));
 });
--- src/app/vehicles/vehicles/filter-form/filter-form.component.ts
+++ src/app/vehicles/vehicles/filter-form/filter-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Filter } from '../../models/filter';
 
 @Component({
   selector: 'app-filter-form',
   templateUrl: './filter-form.component.html',
   styleUrls: ['./filter-form.component.scss']
 })
-export class FilterFormComponent implements OnInit {
+export class FilterFormComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  filtersForm;
+  @Input() types: string[];
+  @Input() brands: string[];
+  @Input() colors: string[];
+  @Output() updateFilter: EventEmitter<Filter> = new EventEmitter<Filter>();
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.filtersForm = this.formBuilder.group({
+      type: '',
+      brand: '',
+      color: '',
+    });
+    this.filtersForm.disable();
+  }
 
   ngOnInit() {
+    // dynamically emit filterValues to parent component each time form values are changing
+    this.filtersForm.valueChanges.subscribe((filterValues: Filter) => {
+      this.updateFilter.emit(filterValues);
+    });
+  }
+
+  ngOnChanges() {
+    // dynamically enable or disable the form upon loading value
+    (!!this.types && !!this.brands && !!this.colors) ? this.filtersForm.enable() : this.filtersForm.disable();
   }
 
 }
