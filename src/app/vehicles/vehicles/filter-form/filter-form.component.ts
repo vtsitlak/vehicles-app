9c5a04b3fa51461806715f424e66fe6229407ef3
LAPTOP-ROIEOSRK\vtsitlak <vtsitlak@gmail.com>
2026-01-11 09:50:59 +0100

    feat: move filter form to signal forms

--- src/app/vehicles/vehicles/filter-form/filter-form.component.ts
+++ src/app/vehicles/vehicles/filter-form/filter-form.component.ts
@@ -1,44 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, inject } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UntypedFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { Field, form } from '@angular/forms/signals';
 import { Filter } from '../../models/filter';
 
 @Component({
     selector: 'app-filter-form',
     templateUrl: './filter-form.component.html',
     styleUrls: ['./filter-form.component.scss'],
-    imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatSelectModule]
+    imports: [CommonModule, MatFormFieldModule, MatSelectModule, Field]
 })
-export class FilterFormComponent implements OnInit, OnChanges {
+export class FilterFormComponent {
 
-  private formBuilder = inject(UntypedFormBuilder);
-  filtersForm;
-  @Input() types: string[];
-  @Input() brands: string[];
-  @Input() colors: string[];
-  @Output() updateFilter: EventEmitter<Filter> = new EventEmitter<Filter>();
+  private filterModel = signal<Filter>({ type: '', brand: '', color: '' });
+  filterForm = form(this.filterModel);
+  types = input<string[] | null>(null);
+  brands = input<string[] | null>(null);
+  colors = input<string[] | null>(null);
+  updateFilter = output<Filter>();
 
   constructor() {
-    this.filtersForm = this.formBuilder.group({
-      type: '',
-      brand: '',
-      color: '',
-    });
-    this.filtersForm.disable();
-  }
-
-  ngOnInit() {
-    // dynamically emit filterValues to parent component each time form values are changing
-    this.filtersForm.valueChanges.subscribe((filterValues: Filter) => {
-      this.updateFilter.emit(filterValues);
+    effect(() => {
+      this.updateFilter.emit(this.filterForm().value());
     });
   }
-
-  ngOnChanges() {
-    // dynamically enable or disable the form upon loading value
-    (!!this.types && !!this.brands && !!this.colors) ? this.filtersForm.enable() : this.filtersForm.disable();
-  }
-
 }
